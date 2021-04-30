2d5cff01cec71b08603137f51d81cfc4d7ab64c4
silentgwh <invisiblegwh@gmail.com>
2021-04-30 07:43:29 +0300

    Serving static content from the API

--- API/Startup.cs
+++ API/Startup.cs
@@ -47,7 +47,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseStaticFiles();
+            
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
